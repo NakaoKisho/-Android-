899fd0320f81582d7c1423c25597f45816a43846
kisho <nk.110181@icloud.com>
2023-06-18 15:15:07 +0900

    Issue「ヘッダーの実装」の実装 画像アップロード未実施(確認中)

--- feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListScreen.kt
+++ feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListScreen.kt
@@ -1,9 +1,17 @@
 package com.oishikenko.android.recruitment.feature.list
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.consumedWindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
@@ -12,9 +20,16 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -26,24 +41,83 @@ fun RecipeListScreen(
     viewModel: RecipeListViewModel = hiltViewModel()
 ) {
     val cookingRecords by viewModel.cookingRecords.collectAsStateWithLifecycle()
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                modifier = Modifier,
+//    Scaffold(
+//        topBar = {
+//            TopAppBar(
+//                modifier = Modifier,
+//                backgroundColor = Color.Transparent,
+//                elevation = 0.dp
+//            ) {
+//                Text(text = stringResource(id = R.string.cooking_records_title))
+//            }
+//        }
+//    ) { innerPadding ->
+//        LazyColumn(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(innerPadding)
+//                .consumedWindowInsets(innerPadding)
+//        ) {
+//            item {
+//                Row(
+//                    modifier = Modifier.fillMaxSize(),
+//                    verticalAlignment = Alignment.CenterVertically,
+//                    horizontalArrangement = Arrangement.Center
+//                ) {
+//                    Text(
+//                        text = stringResource(id = R.string.cooking_records_title),
+//                        modifier = Modifier.padding(0.dp, 0.dp, 8.dp, 0.dp),
+//                        color = colorResource(R.color.cooking_records_title_color),
+//                        fontSize = 20.sp,
+//                        fontWeight = FontWeight.W700,
+//                        lineHeight = 29.sp,
+//                    )
+//                    Image(
+//                        modifier = Modifier
+//                            .height(64.dp)
+//                            .width(66.72.dp),
+//                        painter = painterResource(id = R.drawable.cooking_records_image),
+//                        contentDescription = ""
+//                    )
+//                }
+//            }
+//            items(cookingRecords) {
+//                RecipeListItem(it)
+//            }
+//        }
+//    }
+
+    LazyColumn(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        item {
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
             ) {
-                Text(text = stringResource(id = R.string.cooking_records_title))
+                Text(
+                    text = stringResource(id = R.string.cooking_records_title),
+                    modifier = Modifier.padding(0.dp, 0.dp, 8.dp, 0.dp),
+                    color = colorResource(R.color.cooking_records_title_color),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.W700,
+                    lineHeight = 29.sp,
+                )
+                Image(
+                    modifier = Modifier
+                        .height(64.dp)
+                        .width(66.72.dp),
+                    painter = painterResource(id = R.drawable.cooking_records_image),
+                    contentDescription = ""
+                )
             }
         }
-    ) { innerPadding ->
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(innerPadding)
-                .consumedWindowInsets(innerPadding)
-        ) {
-            items(cookingRecords) {
-                RecipeListItem(it)
-            }
+        items(cookingRecords) {
+            RecipeListItem(it)
         }
     }
 }
