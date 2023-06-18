7d6f88c1d62dd980d342b9f6d3b3d591c0ce4991
kisho <nk.110181@icloud.com>
2023-06-18 15:27:20 +0900

    Issue「ヘッダーの実装」の修正 Figmaでの動きに合わせるためScaffoldを消去 画像アップロード未実施(確認中)

--- feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListScreen.kt
+++ feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListScreen.kt
@@ -3,21 +3,15 @@ package com.oishikenko.android.recruitment.feature.list
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.consumedWindowInsets
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -35,58 +29,12 @@ import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.oishikenko.android.recruitment.feature.R
 
-@OptIn(ExperimentalLayoutApi::class, ExperimentalLifecycleComposeApi::class)
+@OptIn(ExperimentalLifecycleComposeApi::class)
 @Composable
 fun RecipeListScreen(
     viewModel: RecipeListViewModel = hiltViewModel()
 ) {
     val cookingRecords by viewModel.cookingRecords.collectAsStateWithLifecycle()
-//    Scaffold(
-//        topBar = {
-//            TopAppBar(
-//                modifier = Modifier,
-//                backgroundColor = Color.Transparent,
-//                elevation = 0.dp
-//            ) {
-//                Text(text = stringResource(id = R.string.cooking_records_title))
-//            }
-//        }
-//    ) { innerPadding ->
-//        LazyColumn(
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .padding(innerPadding)
-//                .consumedWindowInsets(innerPadding)
-//        ) {
-//            item {
-//                Row(
-//                    modifier = Modifier.fillMaxSize(),
-//                    verticalAlignment = Alignment.CenterVertically,
-//                    horizontalArrangement = Arrangement.Center
-//                ) {
-//                    Text(
-//                        text = stringResource(id = R.string.cooking_records_title),
-//                        modifier = Modifier.padding(0.dp, 0.dp, 8.dp, 0.dp),
-//                        color = colorResource(R.color.cooking_records_title_color),
-//                        fontSize = 20.sp,
-//                        fontWeight = FontWeight.W700,
-//                        lineHeight = 29.sp,
-//                    )
-//                    Image(
-//                        modifier = Modifier
-//                            .height(64.dp)
-//                            .width(66.72.dp),
-//                        painter = painterResource(id = R.drawable.cooking_records_image),
-//                        contentDescription = ""
-//                    )
-//                }
-//            }
-//            items(cookingRecords) {
-//                RecipeListItem(it)
-//            }
-//        }
-//    }
-
     LazyColumn(
         modifier = Modifier
             .background(Color.White)
